da2639d63b70df7e5d3028a204b0928497f755a7
Bland Irony <blandirony@gmail.com>
2025-01-30 16:26:32 +0100

    Initialized home page

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        black: 'var(--color-black)',
+        white: 'var(--color-white)',
+        gray: 'var(--color-gray)'
+      },
+      fontFamily: {
+        'panchang-b': ['Panchang Bold', 'sans-serif'],
+        'panchang-ex': ['Panchang Extrabold', 'sans-serif'],
+        'satoshi': ['Satoshi Regular', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
