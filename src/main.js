87df65241f81267550ad2303b5cba700ee61fb3a
Bland Irony <blandirony@gmail.com>
2025-01-31 04:19:50 +0100

    Completed home page

--- src/main.js
+++ src/main.js
@@ -1,12 +1,20 @@
-import { Item } from "./scripts/items.js";
-import { preloadImages } from "./scripts/utils.js";
+// Libraries
 import Lenis from "lenis";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import barba from '@barba/core'
 
+// Other imports
+import { Item } from "./scripts/items.js";
+import { preloadImages } from "./scripts/utils.js";
+import { products } from "./scripts/db.js";
+
+// Declarations
+const navigation = document.querySelector('nav')
 const linksWrapper = document.querySelector('[data-item="links-wrapper"]')
 const links = linksWrapper.getElementsByTagName('li')
+const anchors = document.querySelectorAll('[data-item="link"]')
+const gallery = document.querySelector('.gallery')
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -25,20 +33,149 @@ gsap.ticker.add((time) => {
 // Disable lag smoothing in GSAP to prevent any delay in scroll animations
 gsap.ticker.lagSmoothing(0);
 
-Array.from(links).forEach(link => {
-  link.addEventListener('mouseover', () => {
-    Array.from(links).forEach(li => {
-      if(li === link) {
-        li.style.color = '#151515'
-      } else {
-        li.style.color = '#a7a7a7'
-      }
-    })
-  })
+export default class Initialize {
+  constructor () {
+    this.init()
+    this.hoverAnimation()
+  }
 
-  link.addEventListener('mouseout', () => {
-    Array.from(links).forEach(li => {
-      li.style.color = '#151515'
+  init () {
+    const tl = gsap.timeline()
+
+    tl.set('.exit', {
+      zIndex: 30,
+      top: 'unset',
+      bottom: 0
     })
-  })
-})
+    .to('.exit', {
+      height: window.innerHeight,
+      duration: 1.25,
+      ease: 'expo.inOut'
+    })
+    .set('.exit', { top: 0, bottom: 'unset' })
+    .from('.exit-span', {
+      yPercent: 100,
+      duration: 1,
+      skewY: 5,
+      stagger: {
+        amount: 0.1
+      },
+      ease: 'power2.out'
+    }, '-=.5')
+    .to('.exit-span', {
+      opacity: 0,
+      duration: 1,
+      ease: 'power2.out'
+    })
+    .to('.exit', {
+      height: 0,
+      duration: 1.5,
+      ease: 'expo.inOut'
+    }, '-=1')
+    .to(navigation, {
+      y: '0',
+      duration: .75,
+      ease: 'power2.inOut'
+    }, '-=.75')
+    .to('[data-item="link"]', {
+      y: 0,
+      duration: 1.75,
+      ease: 'expo.inOut',
+      // delay: 0.045 * i
+      stagger: 0.02
+    }, '-=.5')
+
+    // gsap.utils.toArray('[data-item="link"]').forEach((el, i) => {
+    // })
+  }
+
+  hoverAnimation () {
+    // Links animation and associated animations
+    Array.from(links).forEach((link, index) => {
+      link.addEventListener('mouseover', () => {
+        this.showImages(index)
+        Array.from(links).forEach(li => {
+          if(li === link) {
+            li.style.color = 'var(--color-black)'
+          } else {
+            li.style.color = 'var(--color-gray)'
+          }
+        })
+      })
+      
+      link.addEventListener('mouseout', () => {
+        this.removeImages()
+        Array.from(links).forEach(li => {
+          li.style.color = 'var(--color-black)'
+        })
+      })
+    })
+  }
+
+  showImages (elemIndex) {
+
+    for (let index = 0; index <= 2; index++) {
+      // Create outer container
+      const outer = document.createElement('div')
+      outer.style.gridArea = `${products[elemIndex][`rc${index + 1}`][0]}/${products[elemIndex][`rc${index + 1}`][1]}`
+      
+      // Create image wrapper
+      const imageWrapper = document.createElement('div')
+      imageWrapper.style.width = '100%'
+      imageWrapper.style.height = '100%'
+      imageWrapper.style.clipPath = 'polygon(0% 100%, 100% 100%, 100% 100%, 0% 100%)'
+
+      // Create Image div
+      const image = document.createElement('div')
+      image.style.width = '100%'
+      image.style.height = '100%'
+      image.style.background = `url('${products[elemIndex].imageURLs[index]}')`
+      image.style.backgroundPosition = 'center'
+      image.style.backgroundSize = 'cover'
+      image.style.backgroundRepeat = 'no-repeat'
+
+      // Append Image to wrapper
+      imageWrapper.appendChild(image)
+
+      // Append wrapper to Outer Container
+      outer.appendChild(imageWrapper)
+
+      // Splay image on grid container
+      gallery.appendChild(outer)
+      
+      const tl = gsap.timeline()
+
+      // Animate Images
+      tl.addLabel('start', 0)
+      .to(imageWrapper, {
+        clipPath: 'polygon(0% 100%, 100% 100%, 100% 0%, 0% 0%)',
+        duration: .75,
+        ease: 'power3.out',
+        delay: 0.15 * index
+      })
+      .fromTo(image, { scaleX: 1.12, scaleY: 1.15 }, { scale: 1, duration: .25, ease: 'power2.inOut' }, 'start-=.5')
+
+      console.log(`${products[elemIndex].imageURLs[index]}`);
+    }
+
+  }
+
+  removeImages() {
+    const images = gallery.children;
+  
+    // Animate out each image before removing it
+    Array.from(images).forEach((imageWrapper, index) => {
+      gsap.to(imageWrapper.firstChild, {
+        clipPath: 'polygon(0% 100%, 100% 100%, 100% 100%, 0% 100%)',
+        duration: 0.25,
+        ease: 'power3.in',
+        onComplete: () => {
+          imageWrapper.remove(); // Remove from DOM after animation
+        }
+      });
+    });
+  }
+  
+}
+
+new Initialize()
---
+++ src/scripts/db.js
@@ -0,0 +1,129 @@
+const products = [
+    {
+        name: 'Remy',
+        imageURLs: [
+            './src/assets/images/Remy\ 1.jpg',
+            './src/assets/images/Remy\ 2.jpg',
+            './src/assets/images/Remy\ 3.jpg',
+            './src/assets/images/Remy\ 4.jpg',
+            './src/assets/images/Remy\ 5.jpg',
+            './src/assets/images/Remy\ 6.jpg',
+            './src/assets/images/Remy\ 7.jpg',
+            './src/assets/images/Remy\ 8.jpg',
+            './src/assets/images/Remy\ 9.jpg',
+            './src/assets/images/Remy\ 10.jpg',
+        ],
+        desc: 'This is a description',
+        rc1: ['1/1', '5/3'],
+        rc2: ['3/11', '7/13'],
+        rc3: ['8/3', '12/5']
+    }, {
+        name: 'Romoss',
+        imageURLs: [
+            './src/assets/images/Romoss\ 1.jpg',
+            './src/assets/images/Romoss\ 2.jpg',
+            './src/assets/images/Romoss\ 3.jpg',
+            './src/assets/images/Romoss\ 4.jpg',
+            './src/assets/images/Romoss\ 5.jpg',
+            './src/assets/images/Romoss\ 6.jpg',
+            './src/assets/images/Romoss\ 7.jpg',
+            './src/assets/images/Romoss\ 8.jpg',
+            './src/assets/images/Romoss\ 9.jpg',
+            './src/assets/images/Romoss\ 10.jpg',
+        ],
+        rc1: ['3/2', '6/6'],
+        rc2: ['3/11', '7/13'],
+        rc3: ['8/3', '12/5']
+    }, {
+        name: 'Kriket',
+        imageURLs: [
+            './src/assets/images/Kriket\ 1.jpg',
+            './src/assets/images/Kriket\ 2.jpg',
+            './src/assets/images/Kriket\ 3.jpg',
+            './src/assets/images/Kriket\ 4.jpg',
+            './src/assets/images/Kriket\ 5.jpg',
+            './src/assets/images/Kriket\ 6.jpg',
+            './src/assets/images/Kriket\ 7.jpg',
+            './src/assets/images/Kriket\ 8.jpg',
+            './src/assets/images/Kriket\ 9.jpg',
+            './src/assets/images/Kriket\ 10.jpg',
+        ],
+        desc: 'This is a description',
+        rc1: ['1/1', '5/3'],
+        rc2: ['3/11', '7/13'],
+        rc3: ['8/3', '12/5']
+    }, {
+        name: 'Nuphy Field 75',
+        imageURLs: [
+            './src/assets/images/Nuphy\ Field\ 75\ 1.jpg',
+            './src/assets/images/Nuphy\ Field\ 75\ 2.jpg',
+            './src/assets/images/Nuphy\ Field\ 75\ 3.jpg',
+            './src/assets/images/Nuphy\ Field\ 75\ 4.jpg',
+            './src/assets/images/Nuphy\ Field\ 75\ 5.jpg',
+            './src/assets/images/Nuphy\ Field\ 75\ 6.jpg',
+            './src/assets/images/Nuphy\ Field\ 75\ 7.jpg',
+            './src/assets/images/Nuphy\ Field\ 75\ 8.jpg',
+            './src/assets/images/Nuphy\ Field\ 75\ 9.jpg',
+        ],
+        desc: 'This is a description',
+        rc1: ['1/3', '5/5'],
+        rc2: ['3/9', '6/13'],
+        rc3: ['8/2', '11/6']
+    }, {
+        name: 'NASA',
+        imageURLs: [
+            './src/assets/images/NASA\ 1.jpg',
+            './src/assets/images/NASA\ 2.jpg',
+            './src/assets/images/NASA\ 3.jpg',
+            './src/assets/images/NASA\ 4.jpg',
+            './src/assets/images/NASA\ 5.jpg',
+            './src/assets/images/NASA\ 6.jpg',
+            './src/assets/images/NASA\ 7.jpg',
+            './src/assets/images/NASA\ 8.jpg',
+            './src/assets/images/NASA\ 9.jpg',
+            './src/assets/images/NASA\ 10.jpg',
+        ],
+        desc: 'This is a description',
+        rc1: ['1/1', '5/3'],
+        rc2: ['3/11', '7/13'],
+        rc3: ['8/3', '12/5']
+    }, {
+        name: 'Satellite',
+        imageURLs: [
+            './src/assets/images/Satellite\ 1.jpg',
+            './src/assets/images/Satellite\ 2.jpg',
+            './src/assets/images/Satellite\ 3.jpg',
+            './src/assets/images/Satellite\ 4.jpg',
+            './src/assets/images/Satellite\ 5.jpg',
+            './src/assets/images/Satellite\ 6.jpg',
+            './src/assets/images/Satellite\ 7.jpg',
+            './src/assets/images/Satellite\ 8.jpg',
+        ],
+        desc: 'This is a description',
+        rc1: ['1/1', '5/3'],
+        rc2: ['3/11', '7/13'],
+        rc3: ['8/3', '12/5']
+    }, {
+        name: 'Experientia',
+        imageURLs: [
+            './src/assets/images/Experientia\ 1.jpg',
+            './src/assets/images/Experientia\ 2.jpg',
+            './src/assets/images/Experientia\ 3.jpg',
+            './src/assets/images/Experientia\ 4.jpg',
+            './src/assets/images/Experientia\ 5.jpg',
+            './src/assets/images/Experientia\ 6.jpg',
+            './src/assets/images/Experientia\ 7.jpg',
+            './src/assets/images/Experientia\ 8.jpg',
+            './src/assets/images/Experientia\ 9.jpg',
+            './src/assets/images/Experientia\ 10.jpg',
+        ],
+        desc: 'This is a description',
+        rc1: ['2/1', '6/3'],
+        rc2: ['4/10', '9/12'],
+        rc3: ['8/3', '12/5']
+    }
+]
+
+export {
+    products
+}
